98c3f72e27079bf776e15cc69f1d70acb9176fed
Kouhei Sutou <kou@clear-code.com>
2014-01-27 15:13:48 +0900

    Make host customizable

--- droonga/attributes/default.rb
+++ droonga/attributes/default.rb
@@ -2,6 +2,7 @@ default.droonga.engine.user_name  = "droonga-engine"
 default.droonga.engine.group_name = "droonga-engine"
 default.droonga.engine.home       = "/home/droonga-engine"
 default.droonga.engine.ruby       = "ruby"
+default.droonga.engine.host       = node.name
 default.droonga.engine.port       = 24224
 default.droonga.engine.tag        = "droonga"
 default.droonga.engine.template.cookbooks = {}
