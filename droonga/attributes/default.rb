4e9d4821ddf7da2216924b8e8756cc459beb87bc
YUKI Hiroshi <shimoda@clear-code.com>
2014-04-18 16:13:47 +0900

    Update default port number for the latest droonga-engine

--- droonga/attributes/default.rb
+++ droonga/attributes/default.rb
@@ -3,6 +3,6 @@ default.droonga.engine.group_name = "droonga-engine"
 default.droonga.engine.home       = "/home/droonga-engine"
 default.droonga.engine.ruby       = "ruby"
 default.droonga.engine.host       = node.name
-default.droonga.engine.port       = 24224
+default.droonga.engine.port       = 10031
 default.droonga.engine.tag        = "droonga"
 default.droonga.engine.template.cookbooks = {}
